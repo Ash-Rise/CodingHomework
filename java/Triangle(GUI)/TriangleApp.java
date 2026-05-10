4b62a307d940bc92b20d30e8f7b056dfdd9c8240
AshRise <226440064+Ash-Rise@users.noreply.github.com>
2026-05-11 00:49:30 +0800

    Create TriangleApp.java

---
+++ java/Triangle(GUI)/TriangleApp.java
@@ -0,0 +1,22 @@
+package cn.edu.ncu.lzc;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TriangleApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Triangle.fxml"));
+        primaryStage.setTitle("绘制三角形");
+        primaryStage.setScene(new Scene(root, 550, 400));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
